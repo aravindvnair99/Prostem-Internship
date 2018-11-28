4ae29e64c606fbaee354ff43733845032532985d
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-11-28 17:14:02 +0530

    Signup with recompose

--- prostemintern/src/components/SignUp/index.js
+++ prostemintern/src/components/SignUp/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
@@ -105,7 +106,10 @@ const SignUpLink = () => (
   </p>
 );
 
-const SignUpForm = withRouter(withFirebase(SignUpFormBase));
+const SignUpForm = compose(
+  withRouter,
+  withFirebase,
+)(SignUpFormBase);
 
 export default SignUpPage;
 
