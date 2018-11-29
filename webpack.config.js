aa1da9c8d7a21de95760e507c72832cdfb925973
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-11-29 19:55:47 +0530

    Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,30 @@
 var path = require('path');
 var webpack = require('webpack');
 module.exports = {
- entry: './client/index.js',
- output: {
-  path: path.join(__dirname, 'client'),
-  filename: 'bundle.js'
- },
- module: {
-  loaders: [{
-   test: /.jsx?$/,
-   loader: 'babel-loader',
-   exclude: /node_modules/,
-   query: {
-    presets: ['es2015', 'react']
-   }
-  },
-  {
-   test: /\.css$/,
-   loader: "style-loader!css-loader"
-  }]
- }
+	mode: 'none',
+	entry: './client/index.js',
+	output: {
+		path: path.resolve(__dirname, 'client'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			}, {
+				test: /\.css$/,
+				use: [
+					{ loader: 'style-loader' },
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true
+						}
+					}
+				]
+			}
+		]
+	}
 }
