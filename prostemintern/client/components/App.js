f8f35e47a40a53e88b207fbf2be1cd991cb9cf9a
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-11-29 16:31:42 +0530

    Rearrange

---
+++ prostemintern/client/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Blog</h1>
+        </header>
+        <p className="App-intro">
+          Hey!!!!
+        </p>
+      </div>
+    );
+  }
+}
+export default App;
