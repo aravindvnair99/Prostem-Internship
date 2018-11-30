d8496128d50dd5bf81ac1bf5a4ccfe45b82b54d3
Abhishek Sai <abhishekkandikattu@gmail.com>
2018-11-30 22:48:09 +0530

    Tachyons installed and imported

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom'
 import Routes from './routes'
+import 'tachyons';
 ReactDOM.render(
   <HashRouter>
     <Routes />
